56d562614e29ac7315b5d79ef9fa94822a67b583
TessaYvonne <tessa.vermeir@live.com>
2017-01-24 22:30:23 +0100

    Add files via upload
    nieuwe versie get_text_from_xml + voorbeeld van hoe we boeken kunnen zoeken (search_title)

---
+++ search_title.py
@@ -0,0 +1,10 @@
+#je moet een lusje maken over de lijst met boeken. van elk boek de titel ophalen
+# en dan met str.find() naar de titel zoeken in de thread.
+#str1 is dan xml-tekst en find(str2) is dan de titel van een boek
+
+#voorbeeld:
+str1 = "this is string example....wow!!!"
+str2 = "exam"
+print str1.find(str2)
+print str1.find(str2, 10)
+print str1.find(str2, 40)
